0f34519cac98798855728b1c8c6a4083a4696c59
Liliya Ignatova <liliya.oignatova@gmail.com>
2021-04-21 13:24:41 +0300

    Initial commit

---
+++ controls/api/config.js
@@ -0,0 +1,10 @@
+var firebaseConfig = {
+    apiKey: "AIzaSyBKSWuGdwmN3RaH01U0N3mi5PhRtCyr7Xo",
+    authDomain: "flower-shop-demo-c4c3b.firebaseapp.com",
+    databaseURL: "https://flower-shop-demo-c4c3b-default-rtdb.firebaseio.com",
+    projectId: "flower-shop-demo-c4c3b",
+    storageBucket: "flower-shop-demo-c4c3b.appspot.com",
+    messagingSenderId: "664650686235",
+    appId: "1:664650686235:web:fd367749ba5ab01e5ebdbe"
+};
+firebase.initializeApp(firebaseConfig);
