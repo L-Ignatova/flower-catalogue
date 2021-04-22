3e69f0b0f57ca9cd09d5f2a65218efb7836212c8
Liliya Ignatova <liliya.oignatova@gmail.com>
2021-04-22 17:03:07 +0300

    added get by id

--- controls/api/data.js
+++ controls/api/data.js
@@ -1,9 +1,18 @@
-const apiKey = 'AIzaSyBKSWuGdwmN3RaH01U0N3mi5PhRtCyr7Xo';
 const baseUrl = 'https://flower-shop-demo-c4c3b-default-rtdb.firebaseio.com'
 
 export async function getFlowers() {
     try {
         const response = await fetch(`${baseUrl}/flowers/.json`);
+        const data = await response.json();
+        return data;
+    } catch(err) {
+        throw new Error(err.message);
+    }
+}
+
+export async function getFlowerById(id) {
+    try {
+        const response = await fetch(`${baseUrl}/flowers/${id}.json`);
         const data = await response.json();
         return data;
     } catch(err) {
