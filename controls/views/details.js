aaee3bc537cac09024efe38fa7ce980a8f824244
Liliya Ignatova <liliya.oignatova@gmail.com>
2021-04-22 18:08:52 +0300

    added size choice logic

--- controls/views/details.js
+++ controls/views/details.js
@@ -1,7 +1,7 @@
 import {html} from 'https://unpkg.com/lit-html?module';
 import { getFlowerById, deleteFlower } from "../api/data.js";
 
-const detailsTemplate = (flowerId,hasLiked, flower, isCreator, onDelete, priceChoice, upLike) => html`
+const detailsTemplate = (flowerId, flower, isCreator, onDelete) => html`
 <section class="details-page">
     <div class="wrapper">
         <section class="details-card">
@@ -14,14 +14,13 @@ const detailsTemplate = (flowerId,hasLiked, flower, isCreator, onDelete, priceCh
                     <p><span>Size: </span>
                         <select name="size" id="size-choice">
                             <option value="small">small (3pcs)</option>
-                            <option value="medium">medium (5 pcs)</option>
+                            <option selected="selected" value="medium">medium (5 pcs)</option>
                             <option value="big">big (9pcs)</option>
                         </select>
                     </p>
-                    <p><span>Price: </span>$${priceChoice(flower.price)}</p>
+                    <p id="price"><span>Price: </span>$${flower.price.medium}</p>
                 </div>
                 <div class="buttons">
-                    <p>Likes: ${flower.likes} ${hasLiked ? '' : html`<i @click=${upLike} class="fas fa-heart"></i>`} </p>
                     ${isCreator ? html`
                     <div class="edit-del-btns">
                         <button class="edit"><a href="/edit/${flowerId}">edit</a></button>
@@ -39,26 +38,18 @@ const detailsTemplate = (flowerId,hasLiked, flower, isCreator, onDelete, priceCh
 export async function detailsPage(context) {
     const userId = sessionStorage.getItem('userId');
     const flowerId = context.params.id;
-    const hasLiked = false;
 
     const flower = await getFlowerById(flowerId);
-    if (userId in flower.likes) {
-        hasLiked = true;
-    }
     const isCreator = userId === flower.creator;
 
-    context.render(detailsTemplate(flowerId,hasLiked, flower, isCreator, onDelete, priceChoice, upLike));
-
-    function priceChoice(price) {
-        const choice = document.getElementById('size-choice').value;
-        if (choice == 'small') {
-            return price.small;
-        } else if ( choice == 'medium') {
-            return price.medium;
-        } else {
-            return price.big;
-        }
-    }
+    context.render(detailsTemplate(flowerId, flower, isCreator, onDelete));
+    document.getElementById('size-choice').addEventListener('change', ev => {
+        const priceTag = document.getElementById('price');
+        const element = ev.target;
+        const choice = element.options[element.selectedIndex].text.split(' ')[0];
+        priceTag.innerHTML = `<span>Price: </span>$${flower.price[choice]}`;
+    })
+    
     async function onDelete() {
         const confirmation = confirm('Are you sure you want to delete this flower?');
         if (confirmation) {
@@ -66,7 +57,5 @@ export async function detailsPage(context) {
             context.page.redirect('/catalog');
         }
     }
-    async function upLike() {
-        console.log('one like')
-    }
+
 }
