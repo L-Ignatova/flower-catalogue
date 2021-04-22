e9b1809c8fbc305def7bea66f479a7f8157f12de
Liliya Ignatova <liliya.oignatova@gmail.com>
2021-04-22 16:58:31 +0300

    edited template to added get

--- controls/views/feed.js
+++ controls/views/feed.js
@@ -1,49 +1,34 @@
 import {html, render} from 'https://unpkg.com/lit-html?module';
+import { getFlowers } from "../api/data.js";
 
-const catalogueTemplate = () => html`
+const catalogueTemplate = (flowers) => html`
 <section class="feed">
     <div class="wrapper">
-        <article class="flower-card">
-            <img src="/resources/bouquet_1.jpg" alt="flower 1">
-            <section class="flower-content">
-                <section class="flower-text">
-                    <h4>Majesty palm</h4>
-                    <p>$15</p>
-                </section>
-                <section class="flower-details">
-                    <button class="details-btn"><a href="#">Details</a></button>
-                </section>
-            </section>
-        </article>
-        <article class="flower-card">
-            <img src="/resources/bouquet_2.jpg" alt="flower 1">
-            <section class="flower-content">
-                <section class="flower-text">
-                    <h4>Majesty palm</h4>
-                    <p>$15</p>
-                </section>
-                <section class="flower-details">
-                    <button class="details-btn"><a href="#">Details</a></button>
-                </section>
-            </section>
-        </article>
-        <article class="flower-card">
-            <img src="/resources/bouquet_3.jpg" alt="flower 1">
-            <section class="flower-content">
-                <section class="flower-text">
-                    <h4>Majesty palm</h4>
-                    <p>$15</p>
-                </section>
-                <section class="flower-details">
-                    <button class="details-btn"><a href="#">Details</a></button>
-                </section>
-            </section>
-        </article>
+        ${flowers.length == 0 
+                ? html`<p>No flowers in database.</p>` 
+                : Object.entries(flowers).map(([k, val]) => flowerCard(k, val))}
     </div>
 </section>
 `;
 
-export function cataloguePage(context) {
-    context.render(catalogueTemplate())
+const flowerCard = (flowerKey, values) => html`
+<article class="flower-card">
+    <img src="${values.imageUrl}" alt="${values.name}">
+    <section class="flower-content">
+        <section class="flower-text">
+            <h4>${values.name}</h4>
+            <p>$${values.price.medium}</p>
+        </section>
+        <section class="flower-details">
+            <button class="details-btn"><a href="/details/${flowerKey}">Details</a></button>
+        </section>
+    </section>
+</article>
+`;
+
+export async function cataloguePage(context) {
+    const flowers = await getFlowers();
+    console.log(flowers);
+    context.render(catalogueTemplate(flowers));
 
 }
